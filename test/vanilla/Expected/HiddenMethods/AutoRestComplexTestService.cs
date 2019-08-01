c9c0e8be32693026df46e82ed472254a60418d51
Vaclav Haken <vaclav@vaclavkabat.net>
2019-08-01 15:35:43 +0100

    back to 2.3
    Revert "Revert "Merge pull request #2 from eShopWorld/synclatest""
    
    This reverts commit 475defeb8c12b442155486098a4ca86e4dd5998c.

--- test/vanilla/Expected/HiddenMethods/AutoRestComplexTestService.cs
+++ test/vanilla/Expected/HiddenMethods/AutoRestComplexTestService.cs
@@ -81,6 +81,24 @@ namespace Fixtures.HiddenMethods
         public virtual IReadonlyproperty Readonlyproperty { get; private set; }
 
         /// <summary>
+        /// Gets the IFlattencomplex.
+        /// </summary>
+        public virtual IFlattencomplex Flattencomplex { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the AutoRestComplexTestService class.
+        /// </summary>
+        /// <param name='httpClient'>
+        /// HttpClient to be used
+        /// </param>
+        /// <param name='disposeHttpClient'>
+        /// True: will dispose the provided httpClient on calling AutoRestComplexTestService.Dispose(). False: will not dispose provided httpClient</param>
+        public AutoRestComplexTestService(HttpClient httpClient, bool disposeHttpClient) : base(httpClient, disposeHttpClient)
+        {
+            Initialize();
+        }
+
+        /// <summary>
         /// Initializes a new instance of the AutoRestComplexTestService class.
         /// </summary>
         /// <param name='handlers'>
@@ -167,6 +185,7 @@ namespace Fixtures.HiddenMethods
             Polymorphism = new Polymorphism(this);
             Polymorphicrecursive = new Polymorphicrecursive(this);
             Readonlyproperty = new Readonlyproperty(this);
+            Flattencomplex = new Flattencomplex(this);
             BaseUri = new System.Uri("http://localhost:3000");
             ApiVersion = "2014-04-01-preview";
             SerializationSettings = new JsonSerializerSettings
@@ -182,6 +201,7 @@ namespace Fixtures.HiddenMethods
                         new Iso8601TimeSpanConverter()
                     }
             };
+            SerializationSettings.Converters.Add(new TransformationJsonConverter());
             DeserializationSettings = new JsonSerializerSettings
             {
                 DateFormatHandling = Newtonsoft.Json.DateFormatHandling.IsoDateFormat,
@@ -196,7 +216,10 @@ namespace Fixtures.HiddenMethods
             };
             SerializationSettings.Converters.Add(new PolymorphicSerializeJsonConverter<Fish>("fishtype"));
             DeserializationSettings.Converters.Add(new  PolymorphicDeserializeJsonConverter<Fish>("fishtype"));
+            SerializationSettings.Converters.Add(new PolymorphicSerializeJsonConverter<MyBaseType>("kind"));
+            DeserializationSettings.Converters.Add(new  PolymorphicDeserializeJsonConverter<MyBaseType>("kind"));
             CustomInitialize();
+            DeserializationSettings.Converters.Add(new TransformationJsonConverter());
         }
     }
 }
